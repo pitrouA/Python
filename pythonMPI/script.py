a7b8cedf480c79bef43d7a163c039af9ecac297d
pitrouA <adrien.pitrou@gmail.com>
2021-08-07 15:35:21 +0200

    ajout de MPI for python

---
+++ pythonMPI/script.py
@@ -0,0 +1,16 @@
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+data = {}
+
+if rank == 0:
+    data = {'a': 7, 'b': 3.14}
+    comm.send(data, dest=1, tag=11)
+elif rank == 1:
+    data = comm.recv(source=0, tag=11)
+
+print("rank = "+str(rank)+" : "+str(data))
+
+#mpiexec -n 4 python script.py
